1e9bc6b9edb5d71d29d081dbb4ef06222ba0fbf8
Josh Levinger <josh@levinger.net>
2016-06-14 15:39:16 -0700

    cache results in redis

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, make_response, render_template
 from views import address
+from cache import config_cache
 
 app = Flask(__name__)
 app.config.from_object('app.config')
 app.register_blueprint(address)
+app.cache = config_cache(app)
 
 
 @app.route('/')
--- app/config.py
+++ app/config.py
@@ -1,1 +1,4 @@
-DEBUG = True
+import os
+
+DEBUG = os.environ.get('DEBUG', True)
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/')
