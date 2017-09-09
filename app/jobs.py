2a1e5c43e255adca134700405ec20ff921c6e14c
Josh Levinger <josh@levinger.net>
2017-09-08 19:23:23 -0700

    Store results to redis variables, for admin report

--- app/jobs.py
+++ app/jobs.py
@@ -4,9 +4,15 @@ from integrations import base_crm, briteverify
 import flanker.addresslib.address
 
 import logging
-
+from datetime import datetime
 
 log = logging.getLogger(__name__)
+
+def results_hash(type):
+    return "{date}-{type}".format(
+        date=datetime.now().date(),
+        type=type
+    )
 
 @rq.job
 def validate_new_emails():
@@ -19,8 +25,8 @@ def validate_new_emails():
         simple_validation_job = flanker_validate.queue(email=email)
         save_result_job = save_to_crm.queue(depends_on=simple_validation_job)
 
-    # send admin report
-    # TODO
+    # send admin report after last result is done
+    admin_report_job = send_admin_report.queue(depends_on=save_result_job)
 
 
 @rq.job
@@ -37,8 +43,8 @@ def validate_old_emails():
         if status in ['invalid', 'disposable']:
             crm_instance.unsubscribe(email)
 
-    # send admin report
-    # TODO
+    # send admin report after last result is done
+    admin_report_job = send_admin_report.queue(depends_on=save_result_job)
 
 @rq.job
 def flanker_validate(email):
@@ -54,12 +60,19 @@ def flanker_validate(email):
     data = {'email':email,
             'status':status}
 
-    if data['status'] == 'invalid':
+    # save results to redis hash
+    rq.connection.hincrby(results_hash('flanker'), status)
+
+    if status == 'invalid':
         # try to correct hostname for suggestions
         parsed = flanker.addresslib.address.parse(email)
         suggested_hostname = flanker.addresslib.corrector.suggest(parsed.hostname)
         if suggested_hostname != parsed.hostname:
-            data['suggested_email'] = '%s@%s' % (parsed.mailbox, suggested_hostname)
+            suggested_email = '%s@%s' % (parsed.mailbox, suggested_hostname)
+            log.info('suggestion {}'.format(suggested_email))
+            # save suggestion to redis list
+            rq.connection.append(results_hash('suggestions'), suggested_email)
+            data['suggested_email'] = suggested_email
 
     return data
 
@@ -68,6 +81,10 @@ def briteverify_validate(email):
     # run through briteverify validation
     # more comprehensive, but expensive check
     status = briteverify.check(email)
+
+    # save results to redis hash
+    rq.connection.hincrby(results_hash('briteverify'), status)
+
     return {'email': email, 'status': status}
 
 @rq.job
@@ -97,3 +114,14 @@ def unsubscribe_from_crm():
     log.info('save_to_crm {}:{}'.format(email, status))
     crm_response = crm_instance.unsubscribe_user(email)
     return crm_response
+
+@rq.job
+def send_admin_report():
+    flanker_results = rq.connection.get(results_hash('flanker'))
+    briteverify_results = rq.connection.get(results_hash('briteverify'))
+    suggestions_list = rq.connection.get(results_hash('suggestions'))
+
+    print "flanker_results",flanker_results
+    print "briteverify_results",briteverify_results
+    print "suggestions_list",suggestions_list
+
