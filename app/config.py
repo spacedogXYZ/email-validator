dddd2e71502b8303169ae45e28a6bef1731205d0
Josh Levinger <josh@levinger.net>
2017-09-11 13:40:54 -0700

    Set RQ_REDIS_URL to REDIS_URL, for Flask-RQ2

--- app/config.py
+++ app/config.py
@@ -2,4 +2,5 @@ import os
 
 DEBUG = os.environ.get('DEBUG', False)
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/')
+RQ_REDIS_URL = REDIS_URL
 DNS_TIMEOUT = os.environ.get('DNS_TIMEOUT', 2)  # originally 5.2
