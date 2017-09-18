05e18a8b17a877ba8abe0b12bb1e00152e988fc9
Josh Levinger <josh@levinger.net>
2017-09-18 16:56:47 -0700

    briteverify normalize results

--- app/integrations/briteverify.py
+++ app/integrations/briteverify.py
@@ -5,11 +5,12 @@ BRITEVERIFY_API_URL = 'https://bpi.briteverify.com/emails.json'
 BRITEVERIFY_API_KEY = os.environ.get('BRITEVERIFY_API_KEY')
 
 def check(email):
-    r = requests.get(BRITEVERIFY_API_URL, params={address: email, apikey:BRITEVERIFY_API_KEY})
+    r = requests.get(BRITEVERIFY_API_URL, params={'address': email, 'apikey': BRITEVERIFY_API_KEY})
     response = r.json()
 
     status = response.get('status')
     if response.get('disposable'):
         status = 'disposable'
     # check role_address as well?
-    return status
+    return {'email':email,
+            'status':status}
